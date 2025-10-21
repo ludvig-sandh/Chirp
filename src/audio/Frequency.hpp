e173dc427ad3146db7e2993ad27cae7d362421eb
Ludvig Sandh <givdul11@hotmail.se>
2025-10-21 14:34:45 +0200

    Added the concept of keys and notes to describe frequencies commonly used in music

--- src/audio/Frequency.hpp
+++ src/audio/Frequency.hpp
@@ -2,10 +2,38 @@
 
 #include <algorithm>
 #include <cmath>
+#include <format>
+
+enum Key {
+    A = 0,
+    As,
+    B,
+    C,
+    Cs,
+    D,
+    Ds,
+    E,
+    F,
+    Fs,
+    G,
+    Gs
+};
+
+struct Note {
+    Key key;
+    int octave;
+
+    Note(Key key, int octave = 5) : key(key), octave(octave) {
+        if (octave > 13 || octave < 1) {
+            throw std::invalid_argument(std::format("Cannot create a note from octave {}. Octave must be in range [1, 13].", octave));
+        }
+    }
+};
 
 class Frequency {
 public:
     Frequency(float hertz);
+    Frequency(Note note);
 
     void SetPitch(float pitch);
 
@@ -15,6 +43,8 @@ public:
     float GetAbsolute();
 
 private:
+    static float ConvertNoteToHz(Note note);
+
     float m_hertz = 440;
     float m_pitchBase = 0.0;
     float m_pitchModulation = 0.0;
