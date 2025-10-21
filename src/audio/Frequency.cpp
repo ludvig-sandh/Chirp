4bdedf262d641a24ca997d57c545e115670b374e
Ludvig Sandh <givdul11@hotmail.se>
2025-10-21 14:34:45 +0200

    Added the concept of keys and notes to describe frequencies commonly used in music

--- src/audio/Frequency.cpp
+++ src/audio/Frequency.cpp
@@ -4,7 +4,17 @@
 #include "Frequency.hpp"
 #include "AudioEngine.hpp"
 
-Frequency::Frequency(float hertz) : m_hertz(hertz), m_pitchBase(0.0), m_pitchModulation(0.0) {}
+Frequency::Frequency(float hertz)
+    : m_hertz(hertz)
+    , m_pitchBase(0.0)
+    , m_pitchModulation(0.0)
+{}
+
+Frequency::Frequency(Note note)
+    : m_hertz(Frequency::ConvertNoteToHz(note))
+    , m_pitchBase(0.0)
+    , m_pitchModulation(0.0)
+{}
 
 void Frequency::SetPitch(float pitch) {
     m_pitchBase = pitch;
@@ -23,4 +33,11 @@ float Frequency::GetAbsolute() {
     // Otherwise we'd get foldback aliasing.
     float actual = m_hertz * std::pow(2.0, (m_pitchBase + m_pitchModulation) / m_notesPerOctave);
     return std::min(actual, static_cast<float>(SAMPLE_RATE / 2.0));
+}
+
+float Frequency::ConvertNoteToHz(Note note) {
+    float A5 = 440.0f;
+    float twelvethSq2 = std::pow(2.0f, 1.0f / 12.0f); // Twelveth square root of two
+    float hz = A5 * std::pow(twelvethSq2, note.key) * std::pow(2.0f, static_cast<float>(note.octave - 5));
+    return hz;
 }
