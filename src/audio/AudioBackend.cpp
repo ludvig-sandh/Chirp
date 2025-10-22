fd96591e8c9e7f90200ac9f5245d5d7e9fbbb1e8
Ludvig Sandh <givdul11@hotmail.se>
2025-10-22 17:19:41 +0200

    Added license comment at the top of all source files

--- src/audio/AudioBackend.cpp
+++ src/audio/AudioBackend.cpp
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Ludvig Sandh
+
 #include <stdio.h>
 #include <memory>
 #include <unordered_set>
--- src/gui/GUIManager.hpp
+++ src/gui/GUIManager.hpp
@@ -1,5 +1,7 @@
-#pragma once
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 Ludvig Sandh
 
+#pragma once
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
