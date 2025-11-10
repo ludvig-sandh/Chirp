23dfe1cac8f4a8832913e0638fe3cfd52faf7403
Ludvig Sandh <givdul11@hotmail.se>
2025-11-10 18:29:27 +0100

    GL_SILENCE_DEPRECATION definition handled by CML, not by source code

--- src/gui/GUIManager.hpp
+++ src/gui/GUIManager.hpp
@@ -8,7 +8,6 @@
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_internal.h" // For ImRect
-#define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 #endif
