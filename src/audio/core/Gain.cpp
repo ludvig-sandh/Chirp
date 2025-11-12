8060898535b80f6d8a62baed5aec47f6113231c7
Ludvig Sandh <givdul11@hotmail.se>
2025-11-12 08:05:45 +0100

    Cleaned up the Gain class

--- src/audio/core/Gain.cpp
+++ src/audio/core/Gain.cpp
@@ -7,21 +7,13 @@
 #include <algorithm>
 
 void Gain::SetLinear(float linear) {
-    m_targetLinear = std::clamp(linear, s_minLinear, s_maxLinear);
+    m_targetLinear = std::clamp(linear, MIN_LINEAR, MAX_LINEAR);
 }
 
 void Gain::SetDecibels(float dB) {
-    float clamped = std::clamp(dB, s_minDecibels, s_maxDecibels);
+    float clamped = std::clamp(dB, MIN_DECIBELS, MAX_DECIBELS);
     float linear = std::pow(10.0f, clamped / 20.0f);
     m_targetLinear = linear;
-}
-
-float Gain::GetLinear() const {
-    return m_currentLinear;
-}
-
-float Gain::GetDecibels() const {
-    return 20.0f * std::log10(std::max(m_currentLinear, 1e-5f)); // Avoid log(0)
 }
 
 void Gain::AddModulationLinear(float linearMod) {
@@ -33,13 +25,13 @@ void Gain::ClearModulations() {
 }
 
 float Gain::Apply(float sample) {
-    float target = std::clamp(m_targetLinear + m_modulation, s_minLinear, s_maxLinear);
-    m_currentLinear += (target - m_currentLinear) * s_alpha;
+    float target = std::clamp(m_targetLinear + m_modulation, MIN_LINEAR, MAX_LINEAR);
+    m_currentLinear += (target - m_currentLinear) * ALPHA;
     return m_currentLinear * sample;
 }
 
 AudioFrame Gain::Apply(const AudioFrame& frame) {
-    float target = std::clamp(m_targetLinear + m_modulation, s_minLinear, s_maxLinear);
-    m_currentLinear += (target - m_currentLinear) * s_alpha;
+    float target = std::clamp(m_targetLinear + m_modulation, MIN_LINEAR, MAX_LINEAR);
+    m_currentLinear += (target - m_currentLinear) * ALPHA;
     return m_currentLinear * frame;
 }
--- src/audio/core/Gain.hpp
+++ src/audio/core/Gain.hpp
@@ -9,14 +9,16 @@
 // The Gain class takes care of this.
 class Gain {
 public:
-    // Updates the target gain
+    // Updates the target gain with a value between 0 and 1
     void SetLinear(float linear);
-    void SetDecibels(float dB);
-    
-    float GetLinear() const;
-    float GetDecibels() const;
 
+    // Updates the target gain with a value between -100.0 and 0.0 decibels
+    void SetDecibels(float dB);
+
+    // Adds a modulation on the gain (linear addition)
     void AddModulationLinear(float linearMod);
+
+    // Removes all modulations on this object
     void ClearModulations();
 
     // Applies the current gain and updates it towards the target gain.
@@ -29,12 +31,12 @@ private:
     float m_targetLinear = 1.0;
     float m_modulation = 0.0f; // Adds to m_targetLinear
 
-    const float s_maxLinear = 1.0f;
-    const float s_minLinear = 0.0f;
-    const float s_maxDecibels = 0.0f;
-    const float s_minDecibels = -100.0f;
+    static constexpr float MAX_LINEAR = 1.0f;
+    static constexpr float MIN_LINEAR = 0.0f;
+    static constexpr float MAX_DECIBELS = 0.0f;
+    static constexpr float MIN_DECIBELS = -100.0f;
 
     // The rate at which currentGain should reach targetGain
     // Eg. 0.01 would mean it closes in by 1% each sample.
-    static inline const float s_alpha = 0.01;
+    static constexpr float ALPHA = 0.01;
 };
