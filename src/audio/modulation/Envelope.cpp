056043e2069041b05b453c4a4c0f9cdb622ee3a2
Ludvig Sandh <givdul11@hotmail.se>
2025-11-12 11:58:13 +0100

    Marked methods noexcept in envelope and use normalizedfloats instead of floats

--- src/audio/modulation/Envelope.cpp
+++ src/audio/modulation/Envelope.cpp
@@ -4,12 +4,11 @@
 #include "audio/modulation/Envelope.hpp"
 #include "audio/engine/AudioEngine.hpp"
 
-Envelope::Envelope(float atk, float hld, float dec, float sus, float rel)
-    : attack(atk), hold(hld), decay(dec), sustain(sus), release(rel) {
-    assert(sus >= 0.0 && sus <= 1.0 && "");
-}
+Envelope::Envelope(float atk, float hld, float dec, NormalizedFloat sus, float rel) noexcept
+    : attack(atk), hold(hld), decay(dec), sustain(sus), release(rel)
+{}
 
-float Envelope::GetNextSample() {
+float Envelope::GetNextSample() noexcept {
     float nextSample = GetNextSampleHelper();
     if (!m_hasBeenReleased) {
         m_lastValueBeforeRelease = nextSample;
@@ -17,7 +16,7 @@ float Envelope::GetNextSample() {
     return nextSample;
 }
 
-float Envelope::GetNextSampleHelper() {
+float Envelope::GetNextSampleHelper() noexcept {
     // Progress time
     m_timeSinceStart += 1.0f / SAMPLE_RATE;
 
@@ -34,28 +33,28 @@ float Envelope::GetNextSampleHelper() {
         return m_timeSinceStart / attack;
     }
     if (m_timeSinceStart < attack + hold) {
-        return 1.0;
+        return 1.0f;
     }
     if (m_timeSinceStart < attack + hold + decay) {
         float timeSinceDecStart = m_timeSinceStart - attack - hold;
-        return 1.0 - (1.0 - sustain) * timeSinceDecStart / decay;
+        return 1.0f - (1.0f - sustain.get()) * timeSinceDecStart / decay;
     }
     return sustain;
 }
 
-void Envelope::Restart() {
+void Envelope::Restart() noexcept {
     m_timeSinceStart = 0.0f;
     m_hasBeenReleased = false;
 }
 
-void Envelope::Release() {
+void Envelope::Release() noexcept {
     if (!m_hasBeenReleased) {
         m_timeSinceStart = 0.0f;
         m_hasBeenReleased = true;
     }
 }
 
-bool Envelope::IsComplete() const {
+bool Envelope::IsComplete() const noexcept {
     if (m_hasBeenReleased) {
         return m_timeSinceStart >= release;
     }
--- src/audio/modulation/Envelope.hpp
+++ src/audio/modulation/Envelope.hpp
@@ -5,33 +5,35 @@
 
 #include <cassert>
 #include "audio/modulation/LFO.hpp"
+#include "util/NormalizedFloat.hpp"
 
 // Models a typical ASDR envelope
 class Envelope : public LFO {
 public:
     // "invisible" envelope, just lets the signal pass through
-    Envelope() : attack(0.0f), hold(0.0f), decay(0.0f), sustain(1.0f), release(0.0f) {}
-    Envelope(float atk, float hld, float dec, float sus, float rel);
+    Envelope() noexcept : attack(0.0f), hold(0.0f), decay(0.0f), sustain(1.0f), release(0.0f) {}
+    Envelope(float atk, float hld, float dec, NormalizedFloat sus, float rel) noexcept;
 
-    float GetNextSample() override;
+    // Returns the amplitude of the amplitude at the current time and progresses time forward by the sample period
+    float GetNextSample() noexcept override;
 
     // Starts the envelope back from the beginning
-    void Restart();
+    void Restart() noexcept;
 
     // Envelope goes into release mode (from sustain to quiet) when a note is released
-    void Release();
+    void Release() noexcept;
 
     // Returns true if the envelope has gone through all stages and will be quiet until it is restarted.
-    bool IsComplete() const;
+    bool IsComplete() const noexcept;
 
     float attack;
     float hold;
     float decay;
-    float sustain;
+    NormalizedFloat sustain;
     float release;
     
 private:
-    float GetNextSampleHelper();
+    float GetNextSampleHelper() noexcept;
 
     float m_timeSinceStart = 0.0;
     bool m_hasBeenReleased = false;
